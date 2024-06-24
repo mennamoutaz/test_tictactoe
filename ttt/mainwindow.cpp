27395cb59da0ab8c9f2edfe48ff84df358a2b117
Menna <menna7hoss@gmail.com>
2024-06-24 18:04:52 +0300

    umpdat mainwindow.cpp

--- ttt/mainwindow.cpp
+++ ttt/mainwindow.cpp
@@ -22,7 +22,12 @@ sqlite3* db;
 std::string timeToString(std::chrono::system_clock::time_point timePoint) {
     std::time_t time = std::chrono::system_clock::to_time_t(timePoint);
     std::tm tm;
-    localtime_s(&tm, &time);  // Use localtime_s to convert time to tm struct in a thread-safe manner
+
+#ifdef _WIN32
+    localtime_s(&tm, &time);  // Use localtime_s on Windows
+#else
+    localtime_r(&time, &tm);  // Use localtime_r on Linux
+#endif
 
     std::ostringstream oss;
     oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");  // Format the time using put_time
